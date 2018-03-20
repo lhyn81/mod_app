d1dd35da9714c96215dbfbfdc54ac7bc9c26b8f7
leunghueo@qq.com <leunghueo@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2018-03-20 11:30:17 +0000

    ready to publish
    git-svn-id: svn://svn4.apicloud.com/A6075764076570/modhelper@9 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/myjs.js
+++ script/myjs.js
@@ -1,12 +1,40 @@
 $(document).ready(function(){
-  $(".modbtn").bind('click',test);
+  // $(".modbtn").bind('click',test);
+  toggle_info();
+  $.ajax({
+  url: "http://127.0.0.1:5000/mobile/show/steam",
+  async:false,
+  dataType: "json",
+  success: function (data){
+            $("#mod_info").html("<p>" + data.modInfo + "</p>");
+            $.each(data.modVar, function(i, order){
+              $("#field_input").append("<label for="+data.modVar[i].varID+">"+data.modVar[i].varName+"：</label>");
+              $("#field_input").append("<input type='text' name="+data.modVar[i].varID+"id="+data.modVar[i].varID+" placeholder="+data.modVar[i].varUnit+"><br>");
+            });
+          }
+    });
 });
+
+function toggle_info(){
+  $(".switch_div").hide();
+  $("#mod_info").show();
+}
+
+function toggle_input(){
+  $(".switch_div").hide();
+  $("#mod_input").show();
+}
+
+function toggle_rlt(){
+  $(".switch_div").hide();
+  $("#mod_rlt").show();
+}
 
 function test(){
   console.info($(this).attr('id'));
   var modname=$(this).attr('id');
   $.ajax({
-  url: "http://127.0.0.1:5000/mobile/show/" + modname,
+  url: "http://171.18.199.3:5000/mobile/show/" + modname,
   async:false,
   dataType: "json",
   success: function (data){
